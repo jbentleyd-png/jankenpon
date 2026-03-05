4a2f71ac666927fcdccdf2af0bd44cf085efe3af
Joshua Bentley <232677218+jbentleyd-png@users.noreply.github.com>
2026-03-05 12:42:11 -0700

    Added basic player input.
    Player can now input Rock, Paper, or Scissors, but there is no text verification yet.

--- j.js
+++ j.js
@@ -10,3 +10,14 @@ console.log(getCompPlay());
 console.log(getCompPlay());
 console.log(getCompPlay());
 console.log(getCompPlay());
+
+/* Get User's Move: */
+function getUserMove() {
+  let play;
+  while (play == undefined || play == "Enter") {
+    play = prompt("Please enter 'Rock', 'Paper', or 'Scissors'", "Enter"); //decent, add in reformatting in a second.
+  }
+  return play;
+}
+
+console.log("Player threw " + getUserMove() + ".");
