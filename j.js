c2333bc27eec6ddd4205fcacf51986cbe65d4dcd
Joshua Bentley <232677218+jbentleyd-png@users.noreply.github.com>
2026-03-06 14:41:17 -0700

    Added Tie Checker.
    Inside the playRound function. Function is working, but we need results.

--- j.js
+++ j.js
@@ -12,11 +12,11 @@ console.log(getCompPlay());
 console.log(getCompPlay());
 
 /* Get User's Move: */
-function getUserMove() {
+function getUserMove(tieMessage = "") {
   let play;
   while (!(play === "Rock" || play === "Paper" || play === "Scissors")) {
     //check for what we want, not what we don't want.
-    play = prompt("Please enter 'Rock', 'Paper', or 'Scissors'"); //decent, add in reformatting in a second.
+    play = prompt(tieMessage + "Please enter 'Rock', 'Paper', or 'Scissors'"); //decent, add in reformatting in a second.
     play = capitalize(play);
   }
   return play;
@@ -33,3 +33,19 @@ function capitalize(word) {
 }
 
 console.log(capitalize("fROg"));
+
+function playRound() {
+  let userMove = getUserMove();
+  let compPlay = getCompPlay();
+  let outcome;
+  if (userMove == compPlay) {
+    //check for ties first.
+    let userMove = getUserMove("It's a tie! Throw again! "); //communicate that the tie happened.
+  }
+}
+
+playRound();
+playRound();
+playRound();
+playRound();
+playRound();
